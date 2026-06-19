850f9e6c08da4427e1164109cd46b7665499545c
Gerardo Araque <gerard.araq@gmail.com>
2026-06-18 19:30:00 -0500

    Feat: Carrusel

---
+++ lib/models/movies.dart
@@ -0,0 +1,13 @@
+class Movies {
+  final int id;
+  final String title;
+  final String description;
+  final String image;
+
+  const Movies({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.image,
+  });
+}
--- lib/presentation/streaming/home.dart
+++ lib/presentation/streaming/home.dart
@@ -1,3 +1,5 @@
+import 'package:am3_taller/utils/constants/movies_list.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +7,29 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Home")));
+    return Scaffold(
+      body: SafeArea(
+        child: CarouselSlider(
+          options: CarouselOptions(height: double.infinity),
+          items: movies.map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(i.image),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Text(i.title, style: TextStyle(fontSize: 16.0)),
+                );
+              },
+            );
+          }).toList(),
+        ),
+      ),
+    );
   }
 }
